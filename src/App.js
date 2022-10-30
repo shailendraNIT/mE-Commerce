c12ab248f268f9e92b6584b342a873d8dee86aef
Shailendra Yadav <shailendersinghyadav@gmail.com>
2022-10-30 09:51:48 +0530

    feat: added home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Header from "./header/header";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Header from "./Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/checkout" element={<>Hi This is checkout page</>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
